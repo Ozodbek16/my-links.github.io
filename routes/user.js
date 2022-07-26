7f1f90b0581d9519d2a3fbfaa7f0f0e4668cfc50
Ozodbek16 <progamer16.06.07@gmail.com>
2022-07-26 10:59:47 +0500

    fear: logout

--- routes/user.js
+++ routes/user.js
@@ -7,4 +7,11 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/logout', async (req, res) => {
+    await req.session.destroy((err) => {
+        if (err) console.log(err);
+        else res.redirect('/')
+    })
+})
+
 module.exports = router
